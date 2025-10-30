ff25e76f37abb17432efdddad975a2daf8849a95
ido karpel <idokarpel@idos-MacBook-Pro.local>
2025-10-30 18:36:17 +0200

    bill

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,16 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		int currentValue = Integer.parseInt(args[0]);
+		int rate = Integer.parseInt(args[1]);
+		int n = Integer.parseInt(args[2]);
+		double nrate = rate/100;
+
+		for (int i = 0; i <= n; i++) {
+			currentValue = currentValue + (currentValue * rate); 
+		}
+		System.out.println("future value:" + currentValue);
+		
 	}
+	j
 }
